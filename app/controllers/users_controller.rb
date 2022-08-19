0eedcecb25d113e7bd567cd2e88af637b0c3466b
Miguel <mige.d@outlook.com>
2022-08-18 20:47:34 -0400

    implement login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,23 @@
 class UsersController < ApplicationController
   def create
     @user = User.new(params.require(:user).permit(:username, :password_digest))
-    session[:user_id] = @user.id
 
     if @user.save
+      session[:user_id] = @user.id
       render json: @user
     else
       head 400
+    end
+  end
+
+  def login
+    @user = User.where(username: params[:username], password_digest: params[:password_digest]).first()
+
+    if @user
+      session[:user_id] = @user.id
+      render json: @user
+    else
+      head 403
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   post 'register', to: 'users#create'
+  post 'login', to: 'users#login'
 end
