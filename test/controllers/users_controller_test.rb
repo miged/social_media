81eb858b5dd9429af2c36e1583a188941921647c
Miguel <mige.d@outlook.com>
2022-08-20 02:29:53 -0400

    add login and registration tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,13 +1,31 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get users_new_url
+  test "should create user" do
+    post "/register",
+    params: { user: { username: "john", password_digest: "p@ssw0rd" } }
+    assert_response :success
+
+    post "/login",
+    params: { username: "john", password_digest: "p@ssw0rd" }
     assert_response :success
   end
 
-  test "should get create" do
-    get users_create_url
+  test "should login user" do
+    post "/login",
+    params: { username: "jane", password_digest: "password123" }
     assert_response :success
+  end
+
+  test "should not create a duplicate user" do
+    post "/register",
+    params: { user: { username: "jane", password_digest: "p@ssw0rd" } }
+    assert_response 400
+  end
+
+  test "should validate password when creating user" do
+    post "/register",
+    params: { user: { username: "john" } }
+    assert_response 400
   end
 end
